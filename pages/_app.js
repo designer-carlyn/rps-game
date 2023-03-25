4521e9fe299742b91faef3eb1eeeb4a1a9650b85
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-25 12:36:39 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import "@fontsource/barlow-semi-condensed/600.css";
+import "@fontsource/barlow-semi-condensed/700.css";
+import "@/styles/layout/index.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
