3861993c3fbc749c7a39f696012dd757dec2e955
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-27 12:18:01 +0800

    update layout for playing status

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "@/styles/helpers/variables.scss";
 import "@/styles/layout/index.scss";
 import "@/styles/pages/home.scss";
 import "@/styles/pages/original.scss";
+import "@/styles/components/chip.scss";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/original.js
+++ pages/original.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import "animate.css";
 
 /** Image **/
 import logoScoreBoard from "../public/static/images/logo.svg";
 import iconPaper from "../public/static/images/icon-paper.svg";
-import iconScissor from "../public/static/images/icon-scissors.svg";
+import iconScissors from "../public/static/images/icon-scissors.svg";
 import iconRock from "../public/static/images/icon-rock.svg";
 
 export default function Original() {
-  const chipElement = ["rock", "paper", "scissor"];
+  const [youPicked, setYouPicked] = useState("scissors");
+  const [housePicked, setHousePicked] = useState("scissors");
+  const [playing, setPlaying] = useState(true);
+  const [youWon, setYouWon] = useState();
+
+  const chipElement = ["rock", "paper", "scissors"];
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
   function chooseChip(event) {
     let name = event.target.name;
-    console.log(name);
+    setYouPicked(name);
+    setPlaying(!playing);
+
+    const housePicking = setInterval(() => {
+      setHousePicked(chipElement[getRandomInt(0, 3)]);
+    }, 75);
+
+    setTimeout(() => {
+      clearInterval(housePicking);
+    }, 2000);
   }
 
   return (
@@ -28,26 +46,66 @@ export default function Original() {
             <h1>12</h1>
           </div>
         </div>
-        <div className="rps-original__picking">
-          <div className="picking-chip paper-chip">
-            <button name="paper" onClick={(e) => chooseChip(e)}></button>
-            <div className="chip-wrapper">
-              <Image src={iconPaper} alt="logo"></Image>
+        {!playing ? (
+          <>
+            <div className="rps-original__picking">
+              <div className="wrapper-chip paper-chip">
+                <button name="paper" onClick={(e) => chooseChip(e)}></button>
+                <div className="chip-image">
+                  <Image src={iconPaper} alt="logo"></Image>
+                </div>
+              </div>
+              <div className="wrapper-chip scissors-chip">
+                <button name="scissors" onClick={(e) => chooseChip(e)}></button>
+                <div className="chip-image">
+                  <Image src={iconScissors} alt="logo"></Image>
+                </div>
+              </div>
+              <div className="wrapper-chip rock-chip">
+                <button name="rock" onClick={(e) => chooseChip(e)}></button>
+                <div className="chip-image">
+                  <Image src={iconRock} alt="logo"></Image>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="picking-chip scissor-chip">
-            <button name="scissor" onClick={(e) => chooseChip(e)}></button>
-            <div className="chip-wrapper">
-              <Image src={iconScissor} alt="logo"></Image>
+          </>
+        ) : null}
+        {playing ? (
+          <>
+            <div className="rps-original__playing">
+              <div className="playing-wrapper">
+                <h1>You Picked</h1>
+                <div
+                  className={`wrapper-chip ${youPicked}-chip wrapper-chip--lg ${
+                    youWon == true ? ` winner-${youPicked}-chip` : ``
+                  }`}
+                >
+                  <div className="chip-image">
+                    <img
+                      src={`/static/images/icon-${youPicked}.svg`}
+                      alt={`icon-${youPicked}`}
+                    ></img>
+                  </div>
+                </div>
+              </div>
+              <div className="playing-wrapper">
+                <h1>The House Picked</h1>
+                <div
+                  className={`wrapper-chip ${housePicked}-chip wrapper-chip--lg ${housePicked}-chip wrapper-chip--lg animate__animated animate__flash ${
+                    youWon == false ? `winner-${housePicked}-chip` : ``
+                  }`}
+                >
+                  <div className="chip-image">
+                    <img
+                      src={`/static/images/icon-${housePicked}.svg`}
+                      alt={`icon-${housePicked}`}
+                    ></img>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="picking-chip rock-chip">
-            <button name="rock" onClick={(e) => chooseChip(e)}></button>
-            <div className="chip-wrapper">
-              <Image src={iconRock} alt="logo"></Image>
-            </div>
-          </div>
-        </div>
+          </>
+        ) : null}
       </div>
     </main>
   );
