c54e6bbbb054ed1ffba86c65b25e00c6e81c331b
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-28 15:52:16 +0800

    update initial state of life

--- components/score-status.js
+++ components/score-status.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Image from "next/image";
 import { BestScoreContext, LifeContext } from "@/context/rps-context";
 
@@ -10,6 +10,15 @@ function ScoreStatus() {
   const [bestScore, setBestScore] = useContext(BestScoreContext);
   const [life, setLife] = useContext(LifeContext);
 
+  useEffect(() => {
+    setTimeout(() => {
+      if (JSON.parse(localStorage.getItem("life")) === 0) {
+        localStorage.setItem("life", 3);
+        setLife(3);
+      }
+    }, 500);
+  }, []);
+
   return (
     <div className="rps-status">
       <div className="status-item">
@@ -19,19 +28,19 @@ function ScoreStatus() {
       <div className="status-item">
         <small>Your {life > 1 ? "Lives" : "Life"}</small>
         <div className="item-life">
-          {life == 3 ? (
+          {life === 3 ? (
             <>
               <Image src={iconLife} alt="icon-life"></Image>
               <Image src={iconLife} alt="icon-life"></Image>
               <Image src={iconLife} alt="icon-life"></Image>
             </>
-          ) : life == 2 ? (
+          ) : life === 2 ? (
             <>
               <Image src={iconLife} alt="icon-life"></Image>
               <Image src={iconLife} alt="icon-life"></Image>
               <Image src={iconLifeOutline} alt="icon-life"></Image>
             </>
-          ) : life == 1 ? (
+          ) : life === 1 ? (
             <>
               <Image src={iconLife} alt="icon-life"></Image>
               <Image src={iconLifeOutline} alt="icon-life"></Image>
@@ -39,9 +48,9 @@ function ScoreStatus() {
             </>
           ) : (
             <>
-              <Image src={iconLifeOutline} alt="icon-life"></Image>
-              <Image src={iconLifeOutline} alt="icon-life"></Image>
-              <Image src={iconLifeOutline} alt="icon-life"></Image>
+              <Image src={iconLife} alt="icon-life"></Image>
+              <Image src={iconLife} alt="icon-life"></Image>
+              <Image src={iconLife} alt="icon-life"></Image>
             </>
           )}
         </div>
