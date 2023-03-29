b54fba86001d71c29e6b2c338fd7a6e18893779c
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-29 16:36:41 +0800

    last update

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>RPS Game</title>
       </Head>
       <main className="rps-game">
-        <h1>Choose mode you want to play.</h1>
+        <h1>Choose the mode you want to play.</h1>
         <div className="rps-game__button">
           <Link href="/original">Original Mode</Link>
           <Link href="/advance">Advance Mode</Link>
