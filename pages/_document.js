d381ea758a9a467a15e63a60599b3757d437d665
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-29 16:03:06 +0800

    added meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Entertain your self by playing RPS."
+        ></meta>
+        <meta property="og:title" content="RPS Game" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Entertain yourself by playing RPS."
+        />
+        <meta
+          property="og:image"
+          content="https://ik.imagekit.io/csdesigner/my_portfolio/recent_works/rps-game_M1OH4Eos.webp?updatedAt=1680076784507"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
